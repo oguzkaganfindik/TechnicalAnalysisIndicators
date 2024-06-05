e314f46de72fdebca5382104a8befbbad78c207d
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-06-05 18:16:39 +0300

    05062024T1816

---
+++ TechnicalAnalysisIndicators/Donchain_Channel_Breakout.py
@@ -0,0 +1,65 @@
+﻿# pip install git+https://github.com/rongardF/tvdatafeed tradingview-screener backtesting
+
+import pandas as pd
+import numpy as np
+from tvDatafeed import TvDatafeed, Interval
+from backtesting import Backtest, Strategy
+from tradingview_screener import get_all_symbols
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+def Donchian_Channel_Breakout(data, window):
+    df = data.copy()
+    df['Upper Channel'] = data['High'].rolling(window=window).max()
+    df['Lower Channel'] = data['Low'].rolling(window=window).min()
+    df['Entry'] = df['Close'] > df['Upper Channel'].shift(1)
+    df['Exit'] = df['Close'] < df['Lower Channel'].shift(1)
+    return df
+
+tv = TvDatafeed()
+Hisseler = get_all_symbols(market='turkey')
+Hisseler = [symbol.replace('BIST:', '') for symbol in Hisseler]
+Hisseler = sorted(Hisseler)
+
+#Raporlama için kullanılacak başlıklar
+Titles = ['Hisse Adı', 'Son Fiyat', 'Kazanma Oranı','Giriş Sinyali', 'Çıkış Sinyali']
+df_signals = pd.DataFrame(columns=Titles)
+
+#Backtest için gerekli class yapısı
+class Strategy(Strategy):
+    def init(self):
+        pass
+    def next(self):
+        if self.data['Entry'] == True and not self.position:
+            self.buy()
+
+        elif self.data['Exit'] == True:
+            self.position.close()
+
+
+for i in range(0,len(Hisseler)):
+    #print(Hisseler[i])
+    try:
+        data = tv.get_hist(symbol=Hisseler[i], exchange='BIST', interval=Interval.in_1_hour, n_bars=1000)
+        data.rename(columns={'open': 'Open', 'high': 'High', 'low': 'Low', 'close': 'Close', 'volume': 'Volume'}, inplace=True)
+        data = data.reset_index()
+        DCB = Donchian_Channel_Breakout(data,window=20)
+        DCB['datetime'] = pd.to_datetime(DCB['datetime'])
+        DCB.set_index('datetime', inplace=True)
+        bt = Backtest(DCB, Strategy, cash=100000, commission=0.002)
+        Stats = bt.run()
+        Buy = False
+        Sell = False
+        Signals = DCB.tail(2)
+        Signals = Signals.reset_index()
+        Buy = Signals.loc[0, 'Entry'] == False and Signals.loc[1, 'Entry'] ==True
+        Sell = Signals.loc[0, 'Exit'] == False and Signals.loc[1, 'Exit'] == True
+        Last_Price = Signals.loc[1, 'Close']
+        L1 = [Hisseler[i],Last_Price, round(Stats.loc['Win Rate [%]'], 2), str(Buy), str(Sell)]
+        df_signals.loc[len(df_signals)] = L1
+        print(L1)
+    except:
+        pass
+
+df_True = df_signals[(df_signals['Giriş Sinyali'] == 'True')]
+print(df_True)
